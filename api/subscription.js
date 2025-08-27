9a4ea1f498883e5a5939be12efbbc696cc7f8a27
Abdullah1223 <72107599+Abdullah1223@users.noreply.github.com>
2025-08-27 20:21:17 +0500

    Update subscription.js

--- api/subscription.js
+++ api/subscription.js
@@ -14,7 +14,7 @@ const stripe = new Stripe(
 
 export default async function handler(req, res) {
   // Handle CORS
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
